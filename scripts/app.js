5a1a72af416c83859440c18c3cca4a7beb74b97f
Giordan Battaglin <gio@giordan.ca>
2015-10-20 16:56:03 -0400

    added pseudocode to app.js

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+//ask for geolocation
+
+//geolocation fills city search box, if no geolocation, they can enter manually
+
+//search box allows for searching description
+
+//drop-down for date
+
+//they can use any combo of date, location, or both
+
+//submit button to initiate search
+
+//display list of matched results
+
