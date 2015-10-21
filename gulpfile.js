15a00acb7233fcf50be47073b9e72b81c5eb9682
Giordan Battaglin <gio@giordan.ca>
2015-10-21 16:39:04 -0400

    added gulp files and updated gitignore

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+//npm i pkg -D
+var gulp = require('gulp'),
+		sass = require('gulp-sass'),
+		concat = require('gulp-concat'),
+		jshint = require('gulp-jshint'),
+		autoprefixer = require('gulp-autoprefixer'),
+		browserSync = require('browser-sync'),
+		reload = browserSync.reload;
+
+//styles task
+
+gulp.task('styles', function(){
+	return gulp.src('styles/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat('style.css'))
+		.pipe(autoprefixer('last 2 versions', 'ie8'))
+		.pipe(gulp.dest('styles/'))
+		.pipe(reload({ stream: true }));
+});
+
+gulp.task('js', function(){
+	return gulp.src('scripts/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(reload({ stream: true }));
+});
+
+gulp.task('bs-task', function(){
+	browserSync.init({
+		server:{
+			baseDir: './'
+		}
+	});
+});
+
+//watch task
+
+gulp.task('watch', function (){
+	gulp.watch('styles/*.scss', ['styles']);
+	gulp.watch('scripts/*.js', ['js']);
+	gulp.watch('*.html', reload);
+});
+
+gulp.task('default', ['bs-task', 'styles', 'js', 'watch']);
